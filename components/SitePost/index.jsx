35d28ffe150785b086dbd72ec89fb1d68ef03e10
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2017-03-26 23:50:20 +0200

    Added SideBar to blogposts

--- components/SitePost/index.jsx
+++ components/SitePost/index.jsx
@@ -6,6 +6,7 @@ import { prefixLink } from 'gatsby-helpers'
 import access from 'safe-access'
 import { config } from 'config'
 import ReadNext from '../ReadNext'
+import SiteSidebar from '../SiteSidebar'
 import './style.css'
 import '../../static/css/highlight.css'
 import ReactDisqusThread from 'react-disqus-thread'
@@ -25,33 +26,13 @@ class SitePost extends React.Component {
 
         const home = (
             <div>
-              <Helmet
-                  meta={[
-                      // {property: "og:type", content: "blog"},
-                      // {property: "og:url", content: URL},
-                      // {property: "og:description", content: post.description},
-                      // {property: "og:title", content: post.title},
-                      // {property: "og:image", content: "http://www.military-history.org/wp-content/uploads/2014/04/Wittgenstein.jpg"}
-                      ]}
-                  link={[
-                      {rel: "canonical", href: URL}
-                  ]}
-                  script={[
-                      {src: "https://hypothes.is/embed.js", type: "text/javascript", async:true},
-                      {type: "application/json", text: "{\"showHighlights\": true}", className:"js-hypothesis-config"}
-                  ]}
-              />
-
               <div className="button-container">
-                <div className="button-box">
-                  <Link className='gohome' to={ prefixLink('/') }>All Articles</Link>
-                </div>
-                <div className="button-box">
-                  <Link className='gohome' to={ edit_url }>Edit this article</Link>
-                </div>
-                <div className="button-box">
-                  <Link className='gohome' to={ history_url }>View edit history</Link>
-                </div>
+                  <div className="button-box">
+                   <Link className='gohome' to={ edit_url }>Edit this article</Link>
+                  </div>
+                  <div className="button-box">
+                    <Link className='gohome' to={ history_url }>View edit history</Link>
+                  </div>
               </div>
           </div>
         )
@@ -67,26 +48,49 @@ class SitePost extends React.Component {
 
         return (
             <div>
-              { home }
-              <div className='blog-single'>
-                <div className='text'>
-                  <h1>{ post.title }</h1>
-                  <div dangerouslySetInnerHTML={ { __html: post.body} } />
-                  <div className='date-published'>
-                    <em>Published { moment(post.date).format('D MMM YYYY') }</em>
+              <Helmet
+                  meta={[
+                      // {property: "og:type", content: "blog"},
+                      // {property: "og:url", content: URL},
+                      // {property: "og:description", content: post.description},
+                      // {property: "og:title", content: post.title},
+                      // {property: "og:image", content: "http://www.military-history.org/wp-content/uploads/2014/04/Wittgenstein.jpg"}
+                      ]}
+                  link={[
+                      {rel: "canonical", href: URL}
+                  ]}
+                  script={[
+                      {src: "https://hypothes.is/embed.js", type: "text/javascript", async:true},
+                      {type: "application/json", text: "{\"showHighlights\": true}", className:"js-hypothesis-config"}
+                  ]}
+                />
+              
+              <SiteSidebar {...this.props}/>
+              <div className='content'>
+                <div className='main'>
+                  <div className='main-inner'>
+                    { home }
+                    <div className='blog-single'>
+                      <div className='text'>
+                        <h1>{ post.title }</h1>
+                        <div dangerouslySetInnerHTML={ { __html: post.body} } />
+                        <div className='date-published'>
+                          <em>Published { moment(post.date).format('D MMM YYYY') }</em>
+                        </div>
+                      </div>
+                      <div className='footer'>
+                        <ReadNext post={ post } {...this.props}/>
+                        <hr></hr>
+                        <p>
+                          { config.siteDescr }
+                          <a href={ config.siteTwitterUrl }>
+                            <br></br> <strong>{ config.siteAuthor }</strong> on Twitter</a>
+                        </p>
+                        { disqus }
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className='footer'>
-                  <ReadNext post={ post } {...this.props}/>
-                  <hr></hr>
-                  <p>
-                    { config.siteDescr }
-                    <a href={ config.siteTwitterUrl }>
-                      <br></br> <strong>{ config.siteAuthor }</strong> on Twitter</a>
-                  </p>
-                  { disqus }
-                </div>
-                
               </div>
             </div>
         );
